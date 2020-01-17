7d50aeab618aefdb366785efbbf3df0af4b7edc7
Mark Albiston <mark.albiston@gmail.com>
2020-01-17 13:26:42 -0700

    part II commit

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -10,7 +10,12 @@ module.exports.createProduct = (request, response) => {
         .catch(err => response.json({ message: "Something went wrong", error: err}));
 };
 module.exports.findAllProducts = (request, response) => {
-    Product.find()
-        .then(allProducts => response.json({products: allProducts}))
+    Product.find({})
+        .then(allProducts => response.json(allProducts))
+        .catch(err => response.json({ message: "Something went wrong", error: err}));
+};
+module.exports.findOneProduct = (request, response) => {
+    Product.findOne({_id:request.params.id})
+        .then(product => response.json(product))
         .catch(err => response.json({ message: "Something went wrong", error: err}));
 };
